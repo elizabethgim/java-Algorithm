ed2bff571ded5fbfea7a5eac94b9a804a0ddb94e
yujin <91349650@aromit.com>
2024-02-23 17:10:39 +0900

    create: bj9095

---
+++ java/src/DP/bj9095/Main.java
@@ -0,0 +1,20 @@
+
+public class Main{
+    public int n;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br. readLine());
+
+        n = Integer.parseInt(stringTokenizer.nextToken());
+        int[] arr = new int[n];
+
+        for(int i=0;i<3;i++){
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+
+
+    }
+
+}
